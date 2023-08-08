aec3d70d43923bd6bfe673970ce3648d991b32c2
abirajs <novalnetchennai@gmail.com>
2023-08-08 12:02:34 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -109,7 +109,7 @@ class NovalnetServiceProvider extends ServiceProvider
                           DataBase $dataBase,
                           EventProceduresService $eventProceduresService)
     {
-         $this->getLogger(__METHOD__)->error('ServiceProviderFile');
+
         // Register the Novalnet payment methods in the payment method container
         $payContainer->register('plenty_novalnet::NOVALNET_SEPA', NovalnetSepaPaymentMethod::class,
             [
