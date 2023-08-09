de7341308615e28c06979dccc9f27639d1644da9
abirajs <novalnetchennai@gmail.com>
2023-08-09 18:39:48 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -283,7 +283,7 @@ class CallbackController extends Controller
 	 $orderDetails = $this->transactionLogData->getTransactionData('orderNo', 448057);
 	foreach($orderDetails as $orderDetail) {
 		$additionalInfo = json_decode($orderDetail->additionalInfo, true);
-                 return $this->renderTemplate($additionalInfo);
+                 return $additionalInfo;
 		$this->getLogger(__METHOD__)->error('$additionalInfo', $additionalInfo);
 	}   
 	    
